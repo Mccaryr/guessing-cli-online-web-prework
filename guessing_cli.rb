d058bf047e4c6ccbf1e514c42e8bf491715565ff
Robert McCary <robert.mccary@yahoo.com>
2018-12-18 01:16:15 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   user_input = gets.chomp
   r = Random.new
-  while user_input =! "exit"
+  # while user_input =! "exit"
 
     r.rand(1..6)
     if user_input == "#{r}"
